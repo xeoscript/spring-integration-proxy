8d43ea9ad8858804562a55d9f2f3de2277d97669
Muhammed K K <ajumell@xeoscript.com>
2025-12-22 23:37:17 +0530

    Add GenericIntegrationProxy for handling API integration requests

---
+++ src/main/java/com/xeoscript/libs/integrationsproxy/services/GenericIntegrationProxy.java
@@ -0,0 +1,51 @@
+package com.xeoscript.libs.integrationsproxy.services;
+
+import com.xeoscript.libs.integrationsproxy.components.*;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.context.request.WebRequest;
+
+@RequiredArgsConstructor
+public final class GenericIntegrationProxy<Request, APIResponse, Response>
+        extends IntegrationProxyService<Request, APIResponse, Response> {
+
+    private final RequestParser<Request> requestParser;
+
+    private final RequestValidator<Request> requestValidator;
+
+    private final APIIntegration<Request, APIResponse> apiIntegration;
+
+    private final ResponseValidator<APIResponse> responseValidator;
+
+    private final ResponseSerializer<Response> responseSerializer;
+
+
+    @Override
+    protected Request parseRequest(WebRequest webRequest) {
+        return requestParser.parse(webRequest);
+    }
+
+    @Override
+    protected void validateRequest(Request request) {
+        requestValidator.isValid(request);
+    }
+
+    @Override
+    protected APIResponse performAPI(Request request) {
+        return apiIntegration.generate(request);
+    }
+
+    @Override
+    protected void validateAPIResponse(APIResponse response) {
+        responseValidator.isValid(response);
+    }
+
+    @Override
+    protected Response generateResponse(Object apiResponse) {
+        return null;
+    }
+
+    @Override
+    protected String serializeResponse(Response response) {
+        return responseSerializer.serialize(response);
+    }
+}
